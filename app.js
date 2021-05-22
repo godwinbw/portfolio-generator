58f473ea65a844110e3a49a60b0d3a4bee3d432c
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-05-22 14:43:16 -0500

    added page generation

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+const fs = require("fs").promises;
+const generatePage = require("./src/page-template.js");
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log("================");
-
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile("index.html", generatePage(name, github))
+  .then(() =>
+    console.log("Portolio complete! Check out index.html to see the results")
+  )
+  .catch((err) => {
+    console.log("we had an error");
+    console.log(err);
+  });
